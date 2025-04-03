27eddfb33e3599824ae65c5a9390c7f76c92f6f8
Your Name <thobiehagbedey@gmail.com>
2025-04-02 22:40:29 -0400

    Add RNASeq Pipeline script for job submission and execution

---
+++ RNASeq Pipeline.sh
@@ -0,0 +1,12 @@
+ssh t922316uhn@h4huhnlogin1.uhnresearch.ca # ssh into server
+git clone https://github.com/pughlab/pipeline-suite.git #this is for cloning, when cloning repo, do it without salloc session initiated
+salloc -c 1 -p short -t 1:0:0 --mem 1G # request a short job if needed, remove -p if not a short session
+module load perl # load perl module or any other module you need
+#RNASEQ PIPELINE
+perl /cluster/home/t922316uhn/pipeline-suite/pughlab_rnaseq_pipeline.pl \ 
+-t /cluster/home/t922316uhn/pipeline-suite/configs/rna_pipeline_config.yaml \ 
+-d /cluster/home/t922316uhn/pipeline-suite/configs/rna_fastq_config.yaml \
+--summarize \
+-c slurm \
+--remove \
+--dry-run # --dry-run is optional, it will not run the pipeline, just show what will be done, remove if you want to run the pipeline
