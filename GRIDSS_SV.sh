db6fe5bfd398bb74fad2ba83117af12a624448c2
Tobi Agbede <97619087+Thobie12@users.noreply.github.com>
2025-07-16 23:40:04 -0400

    Add GRIDSS SV calling script for variant analysis (Ultimagen single end data)

---
+++ GRIDSS_SV.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=gridss_sv_calling
+#SBATCH --output=/cluster/home/t922316uhn/PLO/gridss_%j.out
+#SBATCH --error=/cluster/home/t922316uhn/PLO/gridss_%j.err
+#SBATCH --time=96:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --partition=pughlab
+
+
+module load R
+module load bwa
+
+# Variables
+CRAM="/cluster/projects/pughlab/myeloma/external_data/ultimagen-oicr/Crams/OICRM4CA-07-01-P.cram"
+REF="/cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/ref/WholeGenomeFasta/Homo_sapiens_assembly38.fasta"
+OUT_VCF="/cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/VCF/OICRM4CA-07-01-P.sv.vcf"
+OUT_ASSEMBLY="OICRM4CA-07-01-P.gridss.assembly.bam"
+THREADS=8
+
+echo "Starting GRIDSS run on ${CRAM}"
+date
+
+/cluster/home/t922316uhn/gridss/gridss \
+  --jar /cluster/home/t922316uhn/gridss/gridss-2.13.2-gridss-jar-with-dependencies.jar \
+  --reference "$REF" \
+  --reference "$REF" \
+  --output "$OUT_VCF" \
+  --assembly "$OUT_ASSEMBLY" \
+  --threads "$THREADS" \
+  "$CRAM"
+
+echo "GRIDSS finished"
+date
+
+---
