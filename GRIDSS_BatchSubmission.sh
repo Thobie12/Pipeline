ab85e3eb1d7f51e9c58dbd328630448673f34864
Tobi Agbede <97619087+Thobie12@users.noreply.github.com>
2025-07-20 16:06:53 -0400

    Made edits to correct syntax error

--- GRIDSS_BatchSubmission.sh
+++ GRIDSS_BatchSubmission.sh
@@ -5,17 +5,17 @@ REF="/cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/ref/W
 OUT_VCF="/cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/GRIDSS"
 THREADS=8
 CRAM_DIR="/cluster/projects/pughlab/myeloma/external_data/ultimagen-oicr/Crams"
+LOG_DIR="/cluster/home/t922316uhn/PLO/GRIDSS"
 
 # Loop over each sample
 while read SAMPLE; do
 
   # Submit a separate job per sample
   sbatch <<EOF
-
 #!/bin/bash
-#SBATCH --job-name=gridss_\${SAMPLE}
-#SBATCH --output=/cluster/home/t922316uhn/PLO/GRIDSS/gridss_\${SAMPLE}_%j.out
-#SBATCH --error=/cluster/home/t922316uhn/PLO/GRIDSS/gridss_\${SAMPLE}_%j.err
+#SBATCH --job-name=gridss_${SAMPLE}
+#SBATCH --output=${LOG_DIR}/gridss_${SAMPLE}_%j.out
+#SBATCH --error=${LOG_DIR}/gridss_${SAMPLE}_%j.err
 #SBATCH --time=5-00:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
@@ -31,16 +31,15 @@ date
 
 /cluster/home/t922316uhn/gridss/gridss \
   --jar /cluster/home/t922316uhn/gridss/gridss-2.13.2-gridss-jar-with-dependencies.jar \
-  --reference "\${REF}" \
-  --output \${OUT_VCF}/\${SAMPLE}/\${SAMPLE}.sv.vcf \
-  --assembly \${OUT_VCF}/\${SAMPLE}/\${SAMPLE}.gridss.assembly.bam \
-  --threads "\${THREADS}" \
-  --log /cluster/home/t922316uhn/PLO/GRIDSS/\${SAMPLE}.gridss.log \
-  --workingdir \${OUT_VCF}/\${SAMPLE} \
-  "\${CRAM_DIR}/\${SAMPLE}.cram"
+  --reference "$REF" \
+  --output ${OUT_VCF}/${SAMPLE}/${SAMPLE}.sv.vcf \
+  --assembly ${OUT_VCF}/${SAMPLE}/${SAMPLE}.gridss.assembly.bam \
+  --workingdir ${OUT_VCF}/${SAMPLE} \
+  --threads "$THREADS" \
+  "${CRAM_DIR}/${SAMPLE}.cram"
 
 echo "GRIDSS finished"
 date
 
 EOF
-done < "\$SAMPLE_LIST"
+done < "$SAMPLE_LIST"
