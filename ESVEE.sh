94bacb3348371ee540898ea6d758e32dea5168e8
Tobi Agbede <97619087+Thobie12@users.noreply.github.com>
2025-07-21 22:39:13 -0400

    Refactor ESVEE script to update paths and parameters for depth annotation process

--- ESVEE.sh
+++ ESVEE.sh
@@ -1,44 +1,18 @@
-#!bin/bash
-#ESVEE pre and post
+#!/bin/bash
+#SBATCH --job-name=esvee_depth_annotator
+#SBATCH --output=/cluster/home/t922316uhn/PLO/esvee_depth_annotator_%j.out
+#SBATCH --error=/cluster/home/t922316uhn/PLO/esvee_depth_annotator_%j.err
+#SBATCH --time=24:00:00          
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
 
-#Pre GRIDSS code
-java -cp esvee.jar com.hartwig.hmftools.esvee.prep.PrepApplication \
-  -sample OICRM4CA-07-01-P \
-  -bam_file /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/Bam/OICRM4CA-07-01-P.bam \
-  -ref_genome /cluster/tools/data/genomes/human/GRCh38/iGenomes/Sequence/WholeGenomeFasta/Homo_sapiens_assembly38.fasta \
+module load java    # load Java module if needed
+
+java -cp /cluster/home/t922316uhn/ESVEE/sv-prep_v1.1.jar com.hartwig.hmftools.svprep.depth.DepthAnnotator \
+  -input_vcf /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/GRIDSS/OICRM4CA-07-01-P/OICRM4CA-07-01-P.sv.vcf \
+  -output_vcf /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/ESVEE/OICRM4CA-07-01-P/OICRM4CA-07-01-P.sv.vcf \
+  -samples M4-CA-07-01-P-DNA \
+  -bam_files /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/Bam/OICRM4CA-07-01-P.bam \
+  -ref_genome /cluster/tools/data/genomes/human/hg38/iGenomes/Sequence/WholeGenomeFasta/genome.fa \
   -ref_genome_version 38 \
-  -output_dir /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/ESVEE/ 
-  -known_fusion_bed #include if I can find it
-  -blacklist_bed #include if I can find it
-
-#GRIDSS Code
-module load R
-module load bwa
-BAM="??"
-REF="/cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/ref/WholeGenomeFasta/Homo_sapiens_assembly38.fasta"
-OUT_VCF="/cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/VCF/OICRM4CA-07-01-P.sv.vcf"
-OUT_ASSEMBLY="OICRM4CA-07-01-P.gridss.assembly.bam"
-THREADS=8
-echo "Starting GRIDSS run on ${CRAM}"
-date
-
-/cluster/home/t922316uhn/gridss/gridss \
-  --jar /cluster/home/t922316uhn/gridss/gridss-2.13.2-gridss-jar-with-dependencies.jar \
-  --reference "$REF" \
-  --output "$OUT_VCF" \
-  --assembly "$OUT_ASSEMBLY" \
-  --threads "$THREADS" \
-  "$BAM"
-
-echo "GRIDSS finished"
-date
-
-#Post GRIDSS Code
-java -cp esvee.jar com.hartwig.hmftools.esvee.depth.DepthAnnotator \
-  -input_vcf ${gridss_vcf} \
-  -output_vcf ${final_vcf} \
-  -samples OICRM4CA-07-01-P \
-  -bam_files "${tumor_bam}" \
-  -ref_genome /cluster/tools/data/genomes/human/GRCh38/iGenomes/Sequence/WholeGenomeFasta/Homo_sapiens_assembly38.fasta \
-  -ref_genome_version 38 \
-  -threads 8 \
+  -threads 8
