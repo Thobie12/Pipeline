e79c818813c093ee2553d1d182eecd495d8da7ef
Tobi Agbede <97619087+Thobie12@users.noreply.github.com>
2025-07-23 17:20:32 -0400

    modified code for GATKMutect2 optimized version -submits code per chromosome then manually merge at the end for faster analysis

--- Optimized_GATKMutect2.sh
+++ Optimized_GATKMutect2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=mutect2_chr
-#SBATCH --output=mutect2_chr_%A_%a.out
-#SBATCH --error=mutect2_chr_%A_%a.err
-#SBATCH --time=2-00:00:00
+#SBATCH --output=/cluster/home/t922316uhn/PLO/GATK/mutect2_chr_%A_%a.out
+#SBATCH --error=/cluster/home/t922316uhn/PLO/GATK/mutect2_chr_%A_%a.err
+#SBATCH --time=7-00:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=24G
 #SBATCH --partition=pughlab
@@ -18,15 +18,14 @@ CHR=${CHRS[$SLURM_ARRAY_TASK_ID-1]}
 
 gatk Mutect2 \
   -R /cluster/tools/data/genomes/human/GRCh38/iGenomes/Sequence/WholeGenomeFasta/Homo_sapiens_assembly38.fasta \
-  -I /path/to/your.bam \
-  -tumor YOUR_SAMPLE_NAME \
+  -I /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/Bam/OICRM4CA-07-01-P.bam \
+  -tumor OICRM4CA-07-01-P \
   -L $CHR \
-  --panel-of-normals /path/to/1000g_pon.hg38.vcf.gz \
-  --germline-resource /path/to/af-only-gnomad.hg38.vcf.gz \
-  --f1r2-tar-gz ${CHR}.f1r2.tar.gz \
+  --panel-of-normals /cluster/tools/data/genomes/human/GRCh38/iGenomes/Annotation/GATKBundle/1000g_pon.hg38.vcf.gz \
+  --germline-resource /cluster/tools/data/genomes/human/GRCh38/iGenomes/Annotation/GATKBundle/af-only-gnomad.hg38.vcf.gz  \
+  --f1r2-tar-gz /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/GATK/${CHR}.f1r2.tar.gz \
   --native-pair-hmm-threads 8 \
-  --max-reads-per-alignment-start 20 \
-  -O /output/path/mutect2_${CHR}.vcf.gz
+  -O /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/GATK/OICRM4CA-07-01-P_${CHR}.vcf.gz
 
 
 
