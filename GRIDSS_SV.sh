7298f76e0c7328a8dd1d0953dca9f6a5c6b9b05f
Tobi Agbede <97619087+Thobie12@users.noreply.github.com>
2025-08-04 21:59:28 -0400

    Update GRIDSS script for new CRAM file and output paths

--- GRIDSS_SV.sh
+++ GRIDSS_SV.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=gridss_sv_calling
-#SBATCH --output=/cluster/home/t922316uhn/PLO/gridss_%j.out
-#SBATCH --error=/cluster/home/t922316uhn/PLO/gridss_%j.err
+#SBATCH --output=/cluster/home/t922316uhn/PLO/GRIDSS/gridss_CA-08%j.out
+#SBATCH --error=/cluster/home/t922316uhn/PLO/GRIDSS/gridss_CA-08%j.err
 #SBATCH --time=96:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
@@ -12,9 +12,9 @@ module load R
 module load bwa
 
 # Variables
-CRAM="/cluster/projects/pughlab/myeloma/external_data/ultimagen-oicr/Crams/OICRM4CA-07-01-P.cram"
+CRAM="/cluster/projects/pughlab/myeloma/external_data/ultimagen-oicr/Crams/OICRM4CA-08-R-P.cram"
 REF="/cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/ref/WholeGenomeFasta/Homo_sapiens_assembly38.fasta"
-OUT_VCF="/cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/VCF/OICRM4CA-07-01-P.sv.vcf"
+OUT_VCF="/cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/GRIDSS/OICRM4CA-08-R-P/OICRM4CA-08-R-P.sv.vcf"
 OUT_ASSEMBLY="OICRM4CA-07-01-P.gridss.assembly.bam"
 THREADS=8
 
