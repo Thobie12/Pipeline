5ab64189a24dab7b0da1ab8f3e59fc19afdf4646
Tobi Agbede <97619087+Thobie12@users.noreply.github.com>
2025-08-11 14:53:34 -0400

    Add GATK script for LearnReadOrientationModel and FilterMutectCalls

---
+++ GATK_LearnReadOrientation+Filter.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH --job-name=LearnReadOrientationModel
+#SBATCH --output=/cluster/home/t922316uhn/PLO/GATK/Learn_CA-07_%j.out
+#SBATCH --error=/cluster/home/t922316uhn/PLO/GRIDSS/Learn_CA-07_%j.err
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=256G
+#SBATCH --partition=all
+
+module load gatk
+
+# Directory containing all chr*.f1r2.tar.gz files
+F1R2_DIR="/cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/GATK/OICRM4CA-07-01-P"
+
+# Build input args for each file
+INPUTS=""
+for f in "${F1R2_DIR}"/chr*.f1r2.tar.gz; do
+  INPUTS+="-I $f "
+done
+
+# Run LearnReadOrientationModel with all inputs
+gatk LearnReadOrientationModel \
+  $INPUTS \
+  -O /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/GATK/OICRM4CA-07-01-P/read-orientation-model.tar.gz
+
+echo "Done!"
+
+
+
+------
+
+#!/bin/bash
+#SBATCH --job-name=LearnReadOrientationModel
+#SBATCH --output=/cluster/home/t922316uhn/PLO/GATK/filtercalls_CA-07_%j.out
+#SBATCH --error=/cluster/home/t922316uhn/PLO/GRIDSS/filtercalls_CA-07_%j.err
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=all
+
+module load gatk
+
+ gatk FilterMutectCalls \
+   -R /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/ref/WholeGenomeFasta/Homo_sapiens_assembly38.fasta \
+   -V /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/GATK/OICRM4CA-07-01-P/OICRM4CA-07-01-P_all.vcf.gz \
+   -ob-priors /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/GATK/OICRM4CA-07-01-P/read-orientation-model.tar.gz \
+   -O  /cluster/projects/pughlab/myeloma/projects/MM_cell_drugs/WGS_Pipeline/GATK/OICRM4CA-07-01-P/filtered.vcf.gz
+
+   echo "Done!"
